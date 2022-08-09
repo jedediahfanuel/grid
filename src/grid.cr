4ee91d26f9d386bd8acea1098d99d357be59aeee
Jeddi212 <jedediahf51@gmail.com>
2022-08-09 14:21:28 +0700

    🧰 -- term-screen

--- src/grid.cr
+++ src/grid.cr
@@ -1,6 +1,28 @@
-# TODO: Write documentation for `Grid`
+# `Grid` is a string grid formatter library
 module Grid
   VERSION = "0.1.0"
-
-  # TODO: Put your code here
+  # Generate grid output with *list* type of `String` as a input parameter
+  #
+  # Example:
+  # ```
+  # Grid.out("")
+  # ```
+  def self.get(list : String)
+    height, width = {38, 169}
+    list = list.strip
+    print list
+  end
+  
+  # Generate grid output with *list* type of `Array(String)` as a input parameter
+  #
+  # Example:
+  # ```
+  # Grid.get("")
+  # ```
+  def self.get(list : Array(String))
+    get(list.join(" "))
+  end
 end
+
+Grid.get("ijiji iji jiji jij ")
+Grid.get(["asdasd", "asdasda", "asdasdsads", "asdsadasd"])
