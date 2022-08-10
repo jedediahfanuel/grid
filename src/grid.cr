48e42ffeacbc16faa5860f21bb12327709b82a34
Jeddi212 <jedediahf51@gmail.com>
2022-08-10 16:47:51 +0700

    📄 ++ docs -> property & def

--- src/grid.cr
+++ src/grid.cr
@@ -4,13 +4,53 @@ class Grid
   
   # Canvas is a variable that holds the cell of each string
   property canvas    = [] of Array(String)
+  
+  # Holds curently max width for every column.
+  # 
+  # Example:
+  # ```
+  # @col_width = [6, 7, 9]   
+  #
+  # # => 1st column width is 6 char
+  # # => 2nd column width is 7 char
+  # # => 3rd column width is 9 char
+  # ```
   property col_width = [] of Int32
+  
+  # Used to hold the column index position
+  # for `rearrange`
   property col_ptr   = 0
+  
+  # Holds the max width of the canvas.
+  # Its defined by the user.
+  # Default value is `24`.
   property max_width = 0
+  
+  # Holds the currently highest row.
+  #
+  # Example:
+  # ```
+  # @canvas = [
+  #   ["a", "b", "c"], #   => column 1 :: has row 3
+  #   ["d", "e"],      #   => column 2 :: has row 2
+  # ]
+  #
+  # # so the highest row is 3 
+  # # and its became the value of @row
+  # ```
   property row       = 0
+  
+  # Holds the list of String from the user.
+  #
+  # Example:
+  # ```
+  # #         this is 1st column   this is 2nd column
+  # 
+  # @list = [ ["str_1", "str_2"] , ["str_9", "str_8"] ]
+  # ```
   property list : Array(String)
   
-  # Initialize grid with *list* type of `Array(String)` as a input parameter
+  # Initialize grid with *list* type of `Array(String)` as a input parameter.
   #
   # Example:
   # ```
@@ -19,7 +59,7 @@ class Grid
   def initialize(@list : Array(String))
   end
   
-  # Initialize grid with *list* type of `String` as a input parameter
+  # Initialize grid with *list* type of `String` as a input parameter.
   #
   # Example:
   # ```
@@ -29,7 +69,7 @@ class Grid
     @list = str.strip.split
   end
   
-  # Generate grid output with *list* type of `Array(String)` as a input parameter
+  # Generate grid output with *list* type of `Array(String)` as a input parameter.
   #
   # Example:
   # ```
@@ -94,7 +134,7 @@ class Grid
   #   end
   # end
   
-  # Flush (reset) the data from these attribute
+  # Flush (reset) the data from these attribute.
   # ```
   # @col_ptr
   # @canvas
@@ -113,6 +153,7 @@ class Grid
     @row = @list.size
   end
   
+  # Rearrange the current column & row due to newly str proceed to the canvas.
   private def rearrange(str_index : Int32, col_count : Int32)
     @row = (str_index+1 / col_count).ceil.to_i
     @canvas.clear
@@ -137,24 +178,63 @@ class Grid
     is_fit
   end
   
+  # Count the delimiter of the whole column.
+  # Example:
+  # ```
+  # Say we have this @col_width
+  # @col_width[, "b", "c"]
+  # delimiter_count   # => 2
+  # ```
   private def delimiter_count : Int32
     col_size = @col_width.size
     col_size <= 1 ? col_size : col_size - 1
   end
   
+  # Count the delimiter of ranged column from range first to the last column.
+  # Example:
+  # ```
+  # ["a", "b", "c"]
+  # delimiter_count(2)  # => ["a", "b"]
+  # ```
   private def delimiter_count(i : Int32) : Int32
     col_size = @col_width[0..-i].size
     col_size < 1 ? col_size : col_size - 1
   end
   
+  # Get nextly width if we insert the newly str to the last column
+  # `@col_width + delimiter_count + str.size`.
   private def get_next_width(str : String) : Int32
     @col_width.sum(0) + delimiter_count + str.size
   end
   
+  # Get nextly width from first column to the specified column if we insert the newly str.
+  # Ignored column width that out of bound.
+  # Index start from last column.
+  # Example:
+  # @col_width = [4, 4, 3, 3]
+  # newly_str = "four"
+  
+  # get_next_width(newly_str, 2)   
+  # # => col_width[0..-2] + delimiter_count(2) + 4
+  # # => (4 + 4 = 3) + 2 + 4
+  # # => 17
   private def get_next_width(str : String, i : Int32) : Int32
     @col_width[0..-i].sum(0) + delimiter_count(i) + str.size
   end
   
+  # Join the *list* with `'\n'` and convert it to the type of String
+  # 
+  # Example:
+  # ```
+  # @list = [
+  #   "str_1", 
+  #   "str_3", 
+  #   "str_2", 
+  #   "str_4", 
+  #   "str_5"
+  # ]
+  # one_column    #   => "str_1\nstr_3\nstr_2\nstr_4\nstr5"
+  # ```
   def one_column : String
     @list.join("\n")
   end
