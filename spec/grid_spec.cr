55c7b6aa27bb635d505200ac47581aa3cf211b2f
Jeddi212 <jedediahf51@gmail.com>
2022-08-11 16:33:55 +0700

    🧪 ++ .flush testing | minor refactor

--- spec/grid_spec.cr
+++ spec/grid_spec.cr
@@ -1,14 +1,14 @@
 require "./spec_helper"
 
-describe "Grid" do
+describe Grid do
 
-  describe "Initialize" do
-    it "Should initialize new Grid class from String" do
+  describe "#initialize" do
+    it "should initialize new Grid class from String" do
       from_string = Grid.new("Ruby Crystal Emerald Sapphire")
       from_string.list.should eq(["Ruby", "Crystal", "Emerald", "Sapphire"])
     end
     
-    it "Should initialize new Grid class from Array(String)" do
+    it "should initialize new Grid class from Array(String)" do
       from_ar_string = Grid.new(["Ruby", "Crystal", "Emerald", "Sapphire"])
       from_ar_string.list.should eq(["Ruby", "Crystal", "Emerald", "Sapphire"])
     end
@@ -17,6 +17,62 @@ describe "Grid" do
       from_string = Grid.new("Ruby Crystal Emerald Sapphire")
       from_ar_string = Grid.new(["Ruby", "Crystal", "Emerald", "Sapphire"])
       from_ar_string.list.should eq(from_string.list)
+    end
+  end
+  
+  describe ".flush" do
+    context "all = false" do
+      grid = create_filled_object
+      grid.flush
+      
+      it "should clear the @canvas" do
+        grid.canvas.should eq([] of Array(String))
+      end
+    
+      it "should clear the @col_width" do
+        grid.col_width.should eq([] of Int32)
+      end
+      
+      it "should clear the @col_height" do
+        grid.col_height.should eq([] of Int32)
+      end
+      
+      it "should reset the @row" do
+        grid.row.should eq(0)
+      end
+      
+      it "should reset the @max_width)" do
+        grid.max_width.should eq(0)
+      end
+    end
+    
+    context "all = true" do 
+      grid = create_filled_object
+      grid.flush(true)
+      
+      it "should clear the @canvas" do
+        grid.canvas.should eq([] of Array(String))
+      end
+    
+      it "should clear the @col_width" do
+        grid.col_width.should eq([] of Int32)
+      end
+      
+      it "should clear the @col_height" do
+        grid.col_height.should eq([] of Int32)
+      end
+      
+      it "should reset the @row" do
+        grid.row.should eq(0)
+      end
+      
+      it "should reset the @max_width)" do
+        grid.max_width.should eq(0)
+      end
+      
+      it "should clear the @list" do
+        grid.list.should eq([] of String)
+      end
     end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,9 @@
 require "spec"
 require "../src/grid"
 
-grid_1 = Grid.new("Rubys Crystals Emeralds Sapphires")
-grid_2 = Grid.new("Ruby Crystal Emerald Sapphire")
-grid_3 = Grid.new(["Java", "Lua", "C#", "Perl", "Kotlin", "ABAB", "Pascal", "Rust", "Zig", "C++", "C", "APL"])
+def create_filled_object
+  temp = Grid.new("a b c d e f g h j i k l m n o p q r s t u v w x y z")
+  temp.virtual_generate
+  temp.virtual_to_canvas
+  return temp
+end
